e585711de238d5c601e10395cdbd1bc0298c8be7
Dang Minh Thang <dmthang.longan@gmail.com>
2023-07-14 23:56:07 +0700

    update UI doctor list

--- WinFormBirdClinic/Admin/frmDoctor.cs
+++ WinFormBirdClinic/Admin/frmDoctor.cs
@@ -11,48 +11,48 @@ using System.Windows.Forms;
 
 namespace WinFormBirdClinic
 {
-    public partial class frmDoctor : Form
-    {
-        public frmDoctor()
-        {
-            InitializeComponent();
-        }
+	public partial class frmDoctor : Form
+	{
+		public frmDoctor()
+		{
+			InitializeComponent();
+		}
 
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
 
-        private void pictureBox4_Click(object sender, EventArgs e)
-        {
-            frmDoctor doctor = new frmDoctor();
-            doctor.StartPosition = FormStartPosition.CenterScreen;
-            doctor.Show();
-            this.Close();
-        }
+		private void pictureBox4_Click(object sender, EventArgs e)
+		{
+			frmDoctor doctor = new frmDoctor();
+			doctor.StartPosition = FormStartPosition.CenterScreen;
+			doctor.Show();
+			this.Close();
+		}
 
-        private void pictureBox5_Click(object sender, EventArgs e)
-        {
-            frmService service = new frmService();
-            service.StartPosition = FormStartPosition.CenterScreen;
-            service.Show();
-            this.Close();
-        }
+		private void pictureBox5_Click(object sender, EventArgs e)
+		{
+			frmService service = new frmService();
+			service.StartPosition = FormStartPosition.CenterScreen;
+			service.Show();
+			this.Close();
+		}
 
-        private void pictureBox7_Click(object sender, EventArgs e)
-        {
-            frmCustomer customer = new frmCustomer();
-            customer.StartPosition = FormStartPosition.CenterScreen;
-            customer.Show();
-            this.Close();
-        }
+		private void pictureBox7_Click(object sender, EventArgs e)
+		{
+			frmCustomer customer = new frmCustomer();
+			customer.StartPosition = FormStartPosition.CenterScreen;
+			customer.Show();
+			this.Close();
+		}
 
-        private void pictureBox9_Click(object sender, EventArgs e)
-        {
-            frmBookinig frmBookinig = new frmBookinig();
-            frmBookinig.StartPosition = FormStartPosition.CenterScreen;
-            frmBookinig.Show();
-            this.Close();
-        }
-    }
+		private void pictureBox9_Click(object sender, EventArgs e)
+		{
+			frmBookinig frmBookinig = new frmBookinig();
+			frmBookinig.StartPosition = FormStartPosition.CenterScreen;
+			frmBookinig.Show();
+			this.Close();
+		}
+	}
 }
